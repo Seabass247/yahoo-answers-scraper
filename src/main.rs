ba6f637938edf3a79adb53c01a7b1260bdb1a243
Seabass247 <sebjfk@gmail.com>
2021-02-07 22:46:09 -0500

    Update css selector

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,14 @@ async fn main() -> Result<(), reqwest::Error> {
 
     // Parses string of HTML as a document
     let fragment = Html::parse_document(&body);
-    // Parses based on the CSS selector ".Clr-b", which is found by using the "InspectorGadget"
+    // Parses based on the CSS selector ".Clr-b", which is found by using the "SelectorGadget"
     // extension in Chrome on a question in the answers page.
-    let questions = Selector::parse(".Clr-b").unwrap();
+    let questions = Selector::parse(".QuestionCard__title___1DKC-").unwrap();
 
     // Iterate over elements matching the selector
     for question in fragment.select(&questions) {
         // Grab the question text from the element's first (index 0) descendent text node
         let question_txt = question.text().collect::<Vec<_>>()[0];
-
         // Only text containing "?" is a valid question, otherwise it's a category or label text
         if question_txt.contains("?") {
             // Do something with the question.
